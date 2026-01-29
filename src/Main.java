11c001d9b56638cb389398e2e13d9e66a85868e6
purusaxena25 <purusaksena03@gmail.com>
2026-01-29 15:58:22 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Secure Student Management System");
+        System.out.println("Backend application using Java and JDBC");
+    }
+}

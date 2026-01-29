ba82bc0d2c57d1e88d304ea8e4e0262cb86300a7
purusaxena25 <purusaksena03@gmail.com>
2026-01-29 15:58:58 +0530

    Create DatabaseConfig.java

---
+++ src/src/DatabaseConfig.java
@@ -0,0 +1,5 @@
+public class DatabaseConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    public static final String USER = "root";
+    public static final String PASSWORD = "password";
+}
